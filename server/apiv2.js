12ab488ead41bbba23ba651ea9136ab0c032d455
Jacek Kopecky <jacek@jacek.cz>
2019-04-05 09:15:56 +0100

    refactor DBv2 into db.js
    Co-Authored-By: Rich Boakes <rich.boakes@port.ac.uk>

--- server/apiv2.js
+++ server/apiv2.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const mysql = require('mysql2/promise');
 const GoogleAuth = require('simple-google-openid');
 
-const config = require('./config');
 const util = require('./util');
-
-const globalConnection = mysql.createConnection(config.mysql);
+const db = require('./db');
 
 const router = express.Router();
 
@@ -14,7 +11,7 @@ router.post('/messages', GoogleAuth.guardMiddleware(), postMessage);
 
 async function getMessages (req, res) {
   try {
-    res.json(await getMessagesFromDB(req.query.since));
+    res.json(await db.getMessages(req.query.since));
   } catch (e) {
     console.error(e);
     res.sendStatus(500);
@@ -30,34 +27,12 @@ async function postMessage (req, res) {
   }
 
   try {
-    await saveMessageInDB(req.body.value, photoUrl);
-    res.json(await getMessagesFromDB());
+    await db.saveMessage(req.body.value, photoUrl);
+    res.json(await db.getMessages());
   } catch (e) {
     console.error(e);
     res.sendStatus(500);
   }
-}
-
-async function saveMessageInDB (msg, url) {
-  const myConn = await globalConnection;
-  return myConn.execute(
-    'INSERT INTO messages (message,url) VALUES (?,?)',
-    [msg, url]);
-}
-
-async function getMessagesFromDB (since) {
-  const myConn = await globalConnection;
-  let query = 'SELECT id, message, url FROM messages ORDER BY id DESC LIMIT 50';
-  if (since != null) {
-    query = myConn.format('SELECT id, message, url FROM messages WHERE id > ? ORDER BY id DESC LIMIT 50', since);
-  }
-  const [rows] = await myConn.execute(query);
-
-  return rows.map((r) => ({
-    id: r.id,
-    message: r.message,
-    url: r.url,
-  }));
 }
 
 module.exports = router;
---
+++ server/db.js
@@ -0,0 +1,32 @@
+
+const mysql = require('mysql2/promise');
+const config = require('./config');
+
+const globalConnection = mysql.createConnection(config.mysql);
+
+async function saveMessage (msg, url) {
+  const myConn = await globalConnection;
+  return myConn.execute(
+    'INSERT INTO messages (message,url) VALUES (?,?)',
+    [msg, url]);
+}
+
+async function getMessages (since) {
+  const myConn = await globalConnection;
+  let query = 'SELECT id, message, url FROM messages ORDER BY id DESC LIMIT 50';
+  if (since != null) {
+    query = myConn.format('SELECT id, message, url FROM messages WHERE id > ? ORDER BY id DESC LIMIT 50', since);
+  }
+  const [rows] = await myConn.execute(query);
+
+  return rows.map((r) => ({
+    id: r.id,
+    message: r.message,
+    url: r.url,
+  }));
+}
+
+module.exports = {
+  getMessages,
+  saveMessage,
+};
